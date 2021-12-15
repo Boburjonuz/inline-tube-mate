7c60e38b28dbcf2e83a4821c58298a05a056a868
Boburjonuz <95110985+Boburjonuz@users.noreply.github.com>
2021-12-15 22:18:46 +0500

    Update presets.py

--- presets.py
+++ presets.py
@@ -14,12 +14,12 @@ class Presets(object):
 <b>/subs</b><code> - to count subscribers</code>
 <b>/send</b><code>  - to broadcast (as reply)</code>
 
-<b>Join:</b><a href='https://t.me/RMProjects'><b> Support</b></a> | Repo\
+<b>Join:</b><a href='https://t.me/vivdeoyuklovchibot_news'><b> Support</b></a> | Repo\
 <a href='https://github.com/m4mallu/inline-tube-mate'><b> LINK</b></a>   
     """
 
     OPTIONS_TXT = "<code>🔰 Select the option 🔰</code>"
-    WELCOME_MSG = "<code>Hello...</code><b>{}</b> 👋\n<code>I'm a YouTube downloader bot with lot's of features. " \
+    WELCOME_MSG = "<code>Salom...</code><b>{}</b> 👋\n<code>Men YouTubedan video va audio yuklab oluvchi botiman. " \
                   "Search inline for videos and click to proceed for download.</code>"
     RESULTS_TXT = "👀 Results:"
     NO_RESULTS = "❌ No Results"
@@ -36,7 +36,7 @@ class Presets(object):
     SHARE_BUTTON_TEXT = "Hi.. 👋\nCheckout : @{username}\nFor search and download TouTube Videos"
     SAVED_THUMB = "<b>✅ Thumbnail Saved Successfully</b>\n<code>This file will be used in next YouTube " \
                   "downloads until you clear it !</code> "
-    WAIT_MESSAGE = "<code>Processing...</code>"
+    WAIT_MESSAGE = "<code>Jarayonda...</code>"
     THUMB_CAPTION = "<code>This image is your current thumbnail, Tap </code><b> DEL THUMB </b><code> if you wish to " \
                     "clear it !</code> "
     NO_THUMB = "❌ Can't find any thumbnail in your local, Please upload an image to set it.."
@@ -46,24 +46,24 @@ class Presets(object):
     FINISHED_PROGRESS_STR = "◼️"
     UN_FINISHED_PROGRESS_STR = "◻️"
     SOURCE_URL = "https://github.com/m4mallu/inline-tube-mate"
-    SUPPORT_URL = "https://t.me/RMProjects"
-    CHECKING_LINK = "⏳ <code>Analyzing your link...</code>"
-    DOWNLOAD_START = "⬇️ <code>Download Initiated...</code>"
-    UPLOAD_START = "⬆️ <code>Upload Initiated...</code>"
-    NOT_DOWNLOADABLE = "<b>This URL is not downloadable !</b> 🙄"
+    SUPPORT_URL = "https://t.me/Hasanovbobur"
+    CHECKING_LINK = "⏳ <code>Havolangiz tahlil qilinmoqda 🔎</code>"
+    DOWNLOAD_START = "⬇️ <code>Yuklab olish boshlandi 📥</code>"
+    UPLOAD_START = "⬆️ <code>Yuborish boshlandi 📤</code>"
+    NOT_DOWNLOADABLE = "<b>Bu URLni yuklab bo'lmaydi! 🚫</b> 🙄"
     CANCEL_PROCESS = "<b>Process Cancelled Successfully</b>  ✅"
     SEND_TEXT = "<b>Processing...</b>\n<i>This message will automatically disappear when the broadcasting is " \
                 "finished</i> "
     REPLY_ERROR = "<i>Use this command as a replay to any telegram message with out any spaces.</i>"
     USERS_LIST = "<b>Total: {}</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
-    WAIT_MSG = "<b>Processing...</b>\n<i>This will take some time...</i>"
+    WAIT_MSG = "<b>Yuklayabman...</b>\n<i>This will take some time...</i>"
     PROMPT_THUMB = "<b>Do you want to set this image as a thumbnail?</b>"
     FORMAT_SELECTION = """
 <code>Title - </code><b>{}</b>
 
-<code>Channel -</code> <a href={}><b>{}</b></a>
-<code>Uploaded On -</code> <b>{}</b>
-<code>Views -</code> <b>{}  |</b>  <code>Rating:</code> <b>{}</b>
+<code>Kanal nomi -</code> <a href={}><b>{}</b></a>
+<code>Yuklangan sana -</code> <b>{}</b>
+<code>Ko'rishlar soni -</code> <b>{}  |</b>  <code>Reyting:</code> <b>{}</b>
 
 👇<code>Select the required format</code>👇
     """
@@ -73,13 +73,13 @@ class Presets(object):
                        "link in your force subscribe chat.</code>"
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | newfilename | username | password"""
-    CUSTOM_CAPTION_UL_FILE = "\xad \xad\n<b>{}</b>\n\n<b>DL Credits: </b><b><a " \
-                             "href='https://github.com/m4mallu'> M4Mallu</a></b> "
+    CUSTOM_CAPTION_UL_FILE = "\xad \xad\n<b>{}</b>\n\n<b>DL 📥 </b><b><a " \
+                             "href='https://github.com/Video_yuklovchi_bot'> @Video_yuklovchi_bot </a></b> "
     RCHD_TG_API_LIMIT = "Detected File Size: {}\n\nSorry. But, I cannot upload files " \
                         "greater than 1.95GB due to Telegram API limitations."
     AD_STRING_TO_REPLACE = "please report this issue on https://yt-dl.org/bug . Make sure you are using the " \
                            "latest version; see  https://yt-dl.org/update  on how to update. Be sure to call " \
                            "youtube-dl with the --verbose flag and include its complete output."
-    INITIAL_MEDIA = "https://telegra.ph/file/c3f88ba6883554654fb4e.png"
+    INITIAL_MEDIA = "Manzilni yuklash jarayonda iltimos kutib turing!"
     ERROR_MEDIA = "https://telegra.ph/file/27fd810c591c884dba11d.jpg"
     SETTINGS = "<code>Need an update in settings?</code>"
